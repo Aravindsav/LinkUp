fb7206a8e1583a2070f6105cb75ff2a383da2484
Aravind Sai <savaramaravindsai16@gmail.com>
2025-05-04 21:49:45 +0530

    For Deployment

---
+++ Backend/src/routes/users.routes.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import {addToHistory, getUerHistory, login, register } from "../controllers/user.controller.js";
+
+
+
+const router = Router();
+
+router.route("/login").post(login);
+router.route("/register").post(register);
+router.route("/add_to_activity").post(addToHistory);
+router.route("/get_all_activity").get(getUerHistory);
+
+
+export default(router);
+
